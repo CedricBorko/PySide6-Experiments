8b24e7e9143fcb108cb4a25a52c294cb2c152bd3
CedricBorko <cedric@borkoland.de>
2023-07-10 19:12:51 +0200

    LayoutDirection

--- button.py
+++ button.py
@@ -1,6 +1,6 @@
 from enum import IntFlag
 import sys
-from PySide6.QtWidgets import QApplication, QPushButton, QLabel, QWidget, QStyle, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QPushButton, QWidget, QVBoxLayout, QGridLayout
 from PySide6.QtGui import QColor, QPainter, QPaintEvent, QEnterEvent, QMouseEvent, QBrush, QPen, QPainterPath, QFontMetrics, QFont
 from PySide6.QtCore import Qt, QRect, QPoint, QEvent, QSize
 
@@ -10,18 +10,18 @@ from utils import load_svg, draw_svg
 class Button(QPushButton):
     primary_color: QColor = QColor("#008f9b")
     secondary_color: QColor = QColor("#ffffff")
-    icon_background_darkness: int = 150
 
-    border_width: int = 0
-    border_radius: tuple = 0, 0, 0, 0
+    icon_background_contrast: float = 1.15
+
+    border_radius: tuple = 4, 4, 4, 4
 
     margin: int = 8
     padding: int = 24
 
-    def __init__(self, text: str, svg_name: str, parent: QWidget = None) -> None:
+    def __init__(self, text: str, svg_name: str = None, parent: QWidget = None) -> None:
         super().__init__(text, parent)
 
-        self._svg = load_svg(svg_name)
+        self._svg = load_svg(svg_name) if svg_name is not None else None
 
         self._hovered = False
         self._pressed = False
@@ -72,10 +72,6 @@ class Button(QPushButton):
         self.secondary_color = color
         self.update()
 
-    def set_border_width(self, width: int) -> None:
-        self.border_width = max(0, width)
-        self.update()
-
     def set_border_radius(self, top_left: int = 0, top_right: int = 0, bottom_right: int = 0, bottom_left: int = 0) -> None:
         self.border_radius = top_left, top_right, bottom_right, bottom_left
         self.update()
@@ -92,7 +88,7 @@ class Button(QPushButton):
         if self._hovered:
             return self.primary_color.darker(120)
         if self.isChecked():
-            return self.primary_color.lighter(105)
+            return self.primary_color.lighter(115)
 
         return self.primary_color
 
@@ -205,12 +201,12 @@ class Button(QPushButton):
             border_radius = tuple(map(lambda br: br / 100 * self.height(), border_radius))
 
         # Top Left
-        path.moveTo(self.rect().topLeft())
+        path.moveTo(self.__svg_rect().topLeft())
 
         path.arcTo(
             QRect(
-                self.margin,
-                self.margin,
+                self.__svg_rect().left(),
+                self.__svg_rect().top(),
                 min(border_radius[0], min(self.height() // 2, self.width() // 2)),
                 min(border_radius[0], min(self.height() // 2, self.width() // 2)),
             ),
@@ -279,25 +275,40 @@ class Button(QPushButton):
     def __svg_rect(self) -> QRect:
         if not self._svg:
             return QRect(self.margin, self.margin, 0, 0)
-        return QRect(self.margin, self.margin, self.height() - self.margin * 2, self.height() - self.margin * 2)
+
+        size = self.height() - self.margin * 2
+        if self.layoutDirection() == Qt.LayoutDirection.LeftToRight:
+            return QRect(self.margin, self.margin, size, size)
+        return QRect(self.rect().right() - size, self.margin, size, size)
 
     def __text_rect(self) -> QRect:
-        return QRect(self.__svg_rect().right(), self.margin, self.width() - (self.margin + self.padding) * 2, self.height() - self.margin * 2)
+        if self.layoutDirection() == Qt.LayoutDirection.LeftToRight:
+            return QRect(
+                self.__svg_rect().right(),
+                self.margin,
+                self.rect().width() - self.__svg_rect().width(),
+                self.height() - self.margin * 2,
+            )
+
+        return QRect(self.margin, self.margin, self.rect().width() - self.__svg_rect().width(), self.height() - self.margin * 2)
 
     def paintEvent(self, event: QPaintEvent) -> None:
         painter = QPainter(self)
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
 
-        if self.border_width == 0:
-            painter.setPen(Qt.PenStyle.NoPen)
 
         background_path = self.__rect_path()
         svg_rect_path = self.__svg_rect_path()
 
+        painter.setPen(Qt.PenStyle.NoPen)
         painter.setBrush(QBrush(self.__get_primary_color()))
         painter.drawPath(background_path)
-        painter.setBrush(QBrush(self.__get_primary_color().darker(self.icon_background_darkness)))
-        painter.drawPath(svg_rect_path)
+
+        painter.setPen(Qt.PenStyle.NoPen)
+
+        if self._svg:
+            painter.setBrush(QBrush(self.__get_primary_color().darker(self.icon_background_contrast * 100)))
+            painter.drawPath(svg_rect_path)
 
         if self.text():
             painter.setRenderHint(QPainter.RenderHint.TextAntialiasing)
@@ -315,10 +326,36 @@ class Button(QPushButton):
 def main():
     app = QApplication(sys.argv)
     w = QWidget()
-    w.setLayout(QVBoxLayout())
-    Button.primary_color = QColor("#34495e")
+    w.setLayout(QGridLayout())
     b1 = Button("Test", "zoom-in")
-    w.layout().addWidget(b1)
+    b1.primary_color = QColor("#34495e")
+
+    b2 = Button("Test", "zoom-out")
+    b2.primary_color = QColor("#32a852")
+
+    b3 = Button("Test", "zap")
+    b3.primary_color = QColor("#f26957")
+
+    b4 = Button("Test", "briefcase")
+    b4.primary_color = QColor("#3265d1")
+
+    b5 = Button("Test", "ascending")
+    b6 = Button("Test", "chevron-down")
+    b7 = Button("Test", "bell")
+    b7.primary_color = QColor("#af51cf")
+
+    b8 = Button("Test", "bluetooth")
+    b8.setCheckable(True)
+
+    w.layout().addWidget(b1, 0, 0)
+    w.layout().addWidget(b2, 0, 1)
+    w.layout().addWidget(b3, 1, 0)
+    w.layout().addWidget(b4, 1, 1)
+    w.layout().addWidget(b5, 2, 0)
+    w.layout().addWidget(b6, 2, 1)
+    w.layout().addWidget(b6, 2, 1)
+    w.layout().addWidget(b7, 3, 0, 1, 2)
+    w.layout().addWidget(b8, 4, 0, 2, 2)
     w.show()
     sys.exit(app.exec())
 
